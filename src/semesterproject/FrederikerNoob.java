60297e542a02d9ebcd11b95e5dbdc5126c490ec1
Uffe <Uffe@10.18.137.54>
2014-03-20 12:27:02 +0100

    Frederik er noob

---
+++ src/semesterproject/FrederikerNoob.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package semesterproject;
+
+/**
+ *
+ * @author Uffe
+ */
+public class FrederikerNoob {
+   Boolean rigtig = true; 
+}
